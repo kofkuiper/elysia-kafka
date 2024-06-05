f098e096752ccc18be0d7adb734d361ef11fc9fe
kofkuiper <kofkuiper@gmail.com>
2024-06-05 22:25:13 +0700

    :sparkles: feat: completed create order api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import prisma from "./db";
 
 const app = new Elysia()
@@ -22,10 +22,63 @@ const app = new Elysia()
     const products = await prisma.product.findMany()
     return { success: true, products }
   })
-
-  .post("/order", async () => {
-    
+  .get("products", async ({ set }) => {
+    try {
+      const products = await prisma.product.findMany()
+      set.status = 200
+      return { success: true, products }
+    } catch (error) {
+      set.status = 500
+      return { success: false, message: error }
+    }
   })
+  .post("/order", async ({ body, set }) => {
+    try {
+      const { userLineId, productId } = body
+
+      const product = await prisma.product.findUniqueOrThrow({ where: { id: productId } })
+      if (product.amount <= 0) {
+        set.status = 404
+        return { success: false, message: "out of stock" }
+      }
+
+      return await prisma.$transaction(async (tx) => {
+
+        // decrease product amount
+        const updatedProduct = await tx.product.update({
+          where: { id: productId },
+          data: {
+            amount: product.amount - 1
+          }
+        })
+        if (updatedProduct.amount < 0) throw "out of stock"
+
+        // create order
+        const order = await tx.order.create({
+          data: {
+            userLineId,
+            productId,
+            type: 'pending'
+          }
+        })
+        console.log(order);
+
+        set.status = 201
+        return { success: true, message: "create product successful" }
+      })
+
+    } catch (error) {
+      set.status = 500
+      return { success: false, message: error }
+    }
+  },
+    {
+      body: t.Object({
+        userLineId: t.String(),
+        productId: t.String()
+      })
+    },
+  )
 
   .listen(3000);
 
